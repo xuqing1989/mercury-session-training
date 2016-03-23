92ead2cab587daba1e96db6b02cb42cd77b8c702
Qing Xu <xuqing.work@gmail.com>
2016-03-23 10:51:00 -0400

    temp commit

--- public/app.js
+++ public/app.js
@@ -4,24 +4,16 @@
         .controller('LoginCtrl', ['$scope', '$http', function($scope, $http){
             var self = this;
             $http.get('/api/login').success(function(resp){
-                if(resp.username){
-                    self.user = resp;
-                }
+                self.loginInfo = resp;
             });
             self.login = function(user){
                 $http.post('/api/login', user).then(function(data){
-                    if(data.data.username) {
-                        self.user = data.data;
-                    }
-                    else {
-                        self.msg = data.data.msg;
-                    }
-                    $scope.nu = undefined;
+                    self.loginInfo = data.data;
                 });
             }
             self.logout = function(){
                 $http.delete('/api/login').success(function(){
-                    self.user = undefined;
+                    self.loginInfo = {};
                 });
             };
     }])
